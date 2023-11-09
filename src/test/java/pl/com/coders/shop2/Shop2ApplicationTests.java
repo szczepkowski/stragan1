de70730a6449812908ac1d4ce8bd1f9c8c6ad178
pszczepkowski <p.szczepkowski87@gmail.com>
2023-11-09 17:46:04 +0100

    Initial commit

---
+++ src/test/java/pl/com/coders/shop2/Shop2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.coders.shop2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shop2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
